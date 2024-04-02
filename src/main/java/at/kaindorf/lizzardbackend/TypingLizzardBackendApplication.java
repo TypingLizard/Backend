60d30f2e9dce58682d63a06712485de3539cc8ac
Alexander Friedl <friald20@htl-kaindorf.at>
2024-04-02 14:12:54 +0200

    basic project

---
+++ src/main/java/at/kaindorf/lizzardbackend/TypingLizzardBackendApplication.java
@@ -0,0 +1,13 @@
+package at.kaindorf.lizzardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypingLizzardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TypingLizzardBackendApplication.class, args);
+    }
+
+}
